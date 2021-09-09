b8c3b971b99e107c8e2ecdb4f42ab33e99b23ac1
zxc469469 <zxc469469@gmail.com>
2021-09-10 00:10:26 +0800

    set

--- bin/demo.dart
+++ bin/demo.dart
@@ -1,20 +1,25 @@
+void addNumberToFirstItem(List list) {
+  list[0].add(1);
+  print(list);
+}
+
 void main(List<String> arguments) {
   final listA = [1, 2, 3, 4];
   final listB = List.filled(3, []);
   final listC = List.generate(3, (_) => []);
-  var tringList = ['vue', 'kotlin', 'dart', 'angular', 'flutter'];
-  var reversed = tringList..add('123');
+  print(listA);
+  print(listB);
+  print(listC);
+  listB[0].add(1);
+  listC[0].add(1);
+  print(listB);
+  print(listC);
 
-  listA.add(1);
-
-  print(tringList.hashCode);
-  print(tringList);
-  print(reversed.hashCode);
-  print(reversed);
-  listB.forEach((element) {
-    print(element.hashCode);
-  });
-  listC.forEach((element) {
-    print(element.hashCode);
+  final setA = {0, 1, 2, 3, 4};
+  print(setA);
+  final listX = [0, 1, 0, 0, 1, 2, 1, 3, 4, 5, 6, 7];
+  final setB = Set.from(listX);
+  setB.forEach((element) {
+    print(element);
   });
 }
