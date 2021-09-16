b0f3b9ad359de0bf1cbb3a7f0ce5ff8fe2732ee2
zxc469469 <zxc469469@gmail.com>
2021-09-17 01:10:00 +0800

    timer

--- bin/demo.dart
+++ bin/demo.dart
@@ -1,5 +1,23 @@
+import 'dart:async';
+
 import 'package:demo/demo.dart';
 
 void main(List<String> arguments) {
-  print('Hello world');
+  Timer(Duration(seconds: 0), () {
+    print('Timer 1');
+  });
+  print('normal print 1 ');
+
+  Future.delayed(Duration(seconds: 1), () {
+    print('future');
+  })
+      .then((value) => print('then 1'))
+      .then((value) => print('then 2'))
+      .then((value) => print('then 3'));
+
+  Timer(Duration(seconds: 0), () {
+    print('Timer 2');
+  });
+
+  print('main end');
 }
