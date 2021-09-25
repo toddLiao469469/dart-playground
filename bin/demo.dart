66dce603ccd43c5436627da83f81442c454a82cc
zxc469469 <zxc469469@gmail.com>
2021-09-25 16:32:57 +0800

    null-safety

--- bin/demo.dart
+++ bin/demo.dart
@@ -1,5 +1,16 @@
 import 'package:demo/demo.dart';
 
-void main(List<String> arguments) {
+String concatString(String input) => input + '---';
+void main(List<String> arguments) async {
   print('Hello world');
+  final foo = Foo();
+  print(foo.apiData?.length);
+  await foo.fecthData();
+  // concatString(foo.apiData); 這行會出錯
+  concatString(foo.apiData!);
+  print(foo.apiData!.length);
+
+  final test = Test('');
+  test.setInitValue();
+  print(test.a);
 }
--- lib/demo.dart
+++ lib/demo.dart
@@ -1,1 +1,17 @@
+class Foo {
+  String? apiData;
+  Future<void> fecthData() async {
+    await Future.delayed(Duration(seconds: 0), () {
+      apiData = 'hello wrold';
+    });
+  }
+}
 
+class Test {
+  late final String a;
+  final String b;
+  Test(this.b);
+  void setInitValue() {
+    a = 'a';
+  }
+}
